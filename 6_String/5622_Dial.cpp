4c7f91ecfcabc5770c75b6a6df4840de718f74ac
Mun Cheol Ho <cjfgh530@naver.com>
2022-09-13 13:18:46 +0900

    Create 5622_Dial.cpp

---
+++ 6_String/5622_Dial.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str;
+	int time = 0;
+	std::cin >> str;
+	for (int i = 0; i < str.length(); i++)
+	{
+		// A ~ Z == 1 ~ 26, 26 /3 =8... 2 Z 는 9니까 3으로 나눈 몫에 + 1을 해주먄 된다.
+		if (str.length() > 0)
+		{
+			time += 2;
+			if (static_cast<int>(str[i] - 65) >= 0) 
+			{
+        // 중간중간 네 알파벳으로 이루어진 부분을 고려해 예외처리
+				if (static_cast<int>(str[i]) - 65 == 18)
+				{
+					time += (static_cast<int>(str[i]) - 65) / 3;
+				}
+				else if (static_cast<int>(str[i]) - 65 == 21)
+				{
+					time += (static_cast<int>(str[i]) - 65) / 3;
+				}
+				else if (static_cast<int>(str[i]) - 65 >= 22)
+				{
+					time += 8;
+				}
+				else {
+					time += (static_cast<int>(str[i]) - 65) / 3 + 1;
+				}
+				//time += (static_cast<int>(str[i]) - 65) / 3 + 1;
+				
+
+			}
+		}
+	}
+	std::cout << time;
+	return 0;
+}
