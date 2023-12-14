0e6c4a38a9cc7bc11376d21438ae672766952151
Mun Cheol Ho <cjfgh530@naver.com>
2023-12-15 05:56:52 +0900

    Create 진법변환2.cpp

---
+++ 7_BasicMath_1/진법변환2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+	int N; // number
+	int B; // binary, decimal ...
+	std::string  result; // return result
+	
+	std::cin >> N >> B;
+	while (N != 0)
+	{
+		int temp = N % B;
+		if (temp > 9)
+		{
+			temp = temp - 10 + 'A'; // 나머지 저장
+			result += temp;
+		}
+		else
+		{
+			result += ('0' + temp);
+		}
+		N /= B;
+	}
+
+	std::reverse(result.begin(), result.end());
+
+
+	std::cout << result << '\n';
+	return 0;
+}
