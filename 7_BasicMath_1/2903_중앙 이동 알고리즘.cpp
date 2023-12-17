2125254f890b524347d3635271979c192d882d77
Mun Cheol Ho <cjfgh530@naver.com>
2023-12-17 16:43:58 +0900

    Update 2903_중앙 이동 알고리즘.cpp

--- 7_BasicMath_1/2903_중앙 이동 알고리즘.cpp
+++ 7_BasicMath_1/2903_중앙 이동 알고리즘.cpp
@@ -5,15 +5,14 @@ int main()
 	int points = 4; // number of points
 
 	int N; // number of trials
-
 	std::cin >> N;
 
 	for (int i = 0; i < N; i++)
 	{
 		points = std::pow(std::sqrt(points) * 2 - 1, 2);
-
 	}
 
 	std::cout << points;
+	
 	return 0;
 }
