267e3a799f71abbaee20696c28396794ed01f16c
Mun Cheol Ho <cjfgh530@naver.com>
2023-12-19 17:44:27 +0900

    Update 5086_배수와 약수.cpp

--- 8_Factor, Multiple and Prime number/5086_배수와 약수.cpp
+++ 8_Factor, Multiple and Prime number/5086_배수와 약수.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+int main()
+{
+	int A, B;
+	std::vector<std::string> results;
 
+	std::cin >> A >> B;
+
+	while (A != 0 || B != 0)
+	{
+		if (B % A == 0)
+			results.push_back("factor\n");
+		else if (A % B == 0)
+			results.push_back("multiple\n");
+		else
+			results.push_back("neither\n");
+		std::cin >> A >> B;
+	}
+
+	for (int i = 0; i < results.size(); i++)
+	{
+		std::cout << results[i];
+	}
+
+	return 0;
+}
