eee26326addd2cf4046a5a51fa64795a33e9f1ce
Mun Cheol Ho <cjfgh530@naver.com>
2023-12-28 02:22:12 +0900

    Create 11653_소인수분해.cpp

---
+++ 8_Factor, Multiple and Prime number/11653_소인수분해.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+int main()
+{
+	std::vector<int> results;
+
+	int n;
+
+	std::cin >> n;
+
+	for (int i = 2; i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			results.push_back(i);
+			n /= i;
+		}
+	}
+
+	if(n != 1)
+		results.push_back(n);
+
+
+	std::sort(results.begin(), results.end());
+	for (auto& i : results)
+		std::cout << i << " ";
+
+	return 0;
+}
