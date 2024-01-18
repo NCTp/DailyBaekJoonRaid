60821daa40428562864b476ee19ddafb1367c82a
Mun Cheol Ho <cjfgh530@naver.com>
2024-01-19 06:44:41 +0900

    Update 2750_수_정렬하기.cpp

--- 11_Sort/2750_수_정렬하기.cpp
+++ 11_Sort/2750_수_정렬하기.cpp
@@ -2,6 +2,26 @@
 #include <vector>
 #include <algorithm>
 
+// 버블정렬. 인전한 두 원소끼리 비교하면서 정렬하는 기법
+// 장점: 구현의 간단함
+// 단점: 비교할 필요가 더이상 없는 원소도 비교 대상에 포함되고,
+//      가장 왼쪽의 원소가 가장 오른쪽으로 이동해야 할 때, 모든 원소들과 값을 비교해야 하기 때문에
+//      자료형의 크기가 커질수록 더 많은 리소스를 필요로 하게 된다.
+void BubbleSort(std::vector<int> &nums)
+{
+	for (int i = nums.size() - 1; i > 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (nums[j] > nums[j + 1])
+			{
+				int temp = nums[j + 1];
+				nums[j + 1] = nums[j];
+				nums[j] = temp;
+			}
+		}
+	}
+}
 int main()
 {
 	std::vector<int> nums;
@@ -13,10 +33,9 @@ int main()
 		int temp;
 		std::cin >> temp;
 		nums.push_back(temp);
-
-		
 	}
-	std::sort(nums.begin(), nums.end());
+	//std::sort(nums.begin(), nums.end());
+	//BubbleSort(nums);
 
 	for (int i = 0; i < nums.size(); i++)
 	{
@@ -24,3 +43,4 @@ int main()
 	}
 	
 }
+
