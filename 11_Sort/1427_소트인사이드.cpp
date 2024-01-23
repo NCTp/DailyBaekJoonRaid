67351124ee8a86bb6ac53c1071259eb560b1e774
Mun Cheol Ho <cjfgh530@naver.com>
2024-01-23 09:40:57 +0900

    Update 1427_소트인사이드.cpp

--- 11_Sort/1427_소트인사이드.cpp
+++ 11_Sort/1427_소트인사이드.cpp
@@ -7,15 +7,18 @@ int main()
 	std::string str;
 	std::cin >> str;
 	std::vector<int> nums1;
-	std::vector<int> nums2;
+
 	for (int i = 0; i < str.length(); i++)
 	{
 		nums1.push_back(static_cast<int>(str[i]) - 48);
 	}
 
-	std::sort(nums1.begin(), nums1.end());
+	std::sort(nums1.begin(), nums1.end(), [](int a, int b) {
+		return a > b;
+		
+		});
 
-	for (int i = nums1.size()-1; i >= 0; i--)
+	for (int i = 0; i < nums1.size(); i++)
 	{
 		std::cout << nums1[i];
 	}
