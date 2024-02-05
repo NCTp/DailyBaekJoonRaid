827a7aa053514b4c38dc334753e5be52e8635779
Mun Cheol Ho <cjfgh530@naver.com>
2024-02-06 03:22:55 +0900

    Create 1764_듣보잡.cpp

---
+++ 12_Set_and_Map/1764_듣보잡.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+int main()
+{
+	int n,m, sum = 0;
+	std::map<std::string, int> deut;
+	std::map<std::string, int> bo;
+
+	std::cin >> n >> m;
+
+	for (int i = 0; i < n + m; i++)
+	{
+		std::string temp;
+		std::cin >> temp;
+		if (i < n)
+		{
+			deut.insert({temp, 0});
+		}
+		else
+		{
+			if (deut.find(temp) != deut.end())
+			{
+				deut[temp] = 1;
+				sum++;
+			}
+
+		}
+	}
+
+	std::cout << sum << '\n';
+
+	for (std::map<std::string, int>::iterator it = deut.begin(); it != deut.end(); ++it)
+	{
+		if(it->second == 1)
+			std::cout << it->first << '\n';
+		
+	}
+}
