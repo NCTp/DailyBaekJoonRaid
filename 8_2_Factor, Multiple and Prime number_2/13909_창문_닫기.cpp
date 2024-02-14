3facdc79a20f43b01bd1eeafeb8e3340e8ae8af5
Mun Cheol Ho <cjfgh530@naver.com>
2024-02-14 09:13:51 +0900

    Create 13909_창문_닫기.cpp

---
+++ 8_2_Factor, Multiple and Prime number_2/13909_창문_닫기.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	int n_sqrt = static_cast<int>(std::sqrt(n));
+	//GetP(n);
+
+	if (n == 1)
+	{
+		std::cout << static_cast<int>(std::sqrt(n));
+	}
+	else
+	{
+		if (n_sqrt == std::sqrt(n))
+		{
+			std::cout << static_cast<int>(std::sqrt(n) - 1);
+		}
+		else
+		{
+			std::cout << static_cast<int>(std::sqrt(n));
+		}
+	}
+}
