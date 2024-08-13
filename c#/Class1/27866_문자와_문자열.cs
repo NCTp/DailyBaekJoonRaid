1c7b2989701c649c2142f23f33c5dc068ae28524
Mun Cheol Ho <cjfgh530@naver.com>
2024-08-14 01:28:41 +0900

    Create 27866_문자와_문자열.cs

---
+++ c#/Class1/27866_문자와_문자열.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+
+namespace BackJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine(S[i - 1]);
+
+        }
+    }
+}
