00843e5c2d9f8ab9533aee3b0ecc5b693564b952
Mun Cheol Ho <cjfgh530@naver.com>
2024-08-14 01:25:39 +0900

    Create 25083_새싹.cs

---
+++ c#/Class1/25083_새싹.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text;
+
+namespace BackJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("         ,r'\"7");
+            stringBuilder.AppendLine("r`-_   ,'  ,/");
+            stringBuilder.AppendLine(" \\. \". L_r'");
+            stringBuilder.AppendLine("   `~\\/");
+            stringBuilder.AppendLine("      |");
+            stringBuilder.AppendLine("      |");
+            
+            
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
