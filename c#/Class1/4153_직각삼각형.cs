4dab7137de5c6a6f3bc4370499e91cd3aae98411
Mun Cheol Ho <cjfgh530@naver.com>
2024-08-16 11:43:26 +0900

    Create 4153_직각삼각형.cs

---
+++ c#/Class1/4153_직각삼각형.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net.Mime;
+using System.Text;
+
+namespace BackJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                String[] length = Console.ReadLine().Split();
+                int length_A = int.Parse(length[0]);
+                int length_B = int.Parse(length[1]);
+                int length_C = int.Parse(length[2]);
+                // 정렬
+                if (length_A >= length_B)
+                {
+                    int temp = length_B;
+                    length_B = length_A;
+                    length_A = temp;
+                }
+                if (length_B >= length_C)
+                {
+                    int temp = length_C;
+                    length_C = length_B;
+                    length_B = temp;
+                }
+
+                if (length_A == 0 && length_B == 0 && length_C == 0) // 프로그램 종료 조건
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    if ((length_A * length_A) + (length_B * length_B) == (length_C * length_C))
+                    {
+                        Console.WriteLine("right");
+                    }
+                    else
+                    {
+                        Console.WriteLine("wrong");
+                    }
+                }
+            }
+        }
+    }
+}
